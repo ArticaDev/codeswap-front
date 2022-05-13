47b7392d9fdebc6bc14735752a6d100576f9b0ea
LL <limalucas454@gmail.com>
2022-05-13 15:23:00 -0300

    🐛 improving rendering logic and text feedback

--- src/components/UserCode.svelte
+++ src/components/UserCode.svelte
@@ -24,20 +24,22 @@
 <h2 class="text-4xl text-orange-500">{title}</h2>
 <div class="border-orange-500 border-2">
   <pre><code class="text-left">{code}</code></pre>
-  <button
-    on:click={() => copyCode()}
-    class="p-2 border-orange-500 border-2 rounded flex justify-center items-center m-2 ml-auto"
-  >
-    {#if copied}
-      <div class="text-green-500 w-4 h-4">
-        <FaCheckDouble />
-      </div>
-    {:else}
-      <div class="text-orange-500 w-4 h-4">
-        <FaRegCopy />
-      </div>
-    {/if}
-  </button>
+  {#if window.isSecureContext}
+    <button
+      on:click={() => copyCode()}
+      class="p-2 border-orange-500 border-2 rounded flex justify-center items-center m-2 ml-auto"
+    >
+      {#if copied}
+        <div class="text-green-500 w-4 h-4">
+          <FaCheckDouble />
+        </div>
+      {:else}
+        <div class="text-orange-500 w-4 h-4">
+          <FaRegCopy />
+        </div>
+      {/if}
+    </button>
+  {/if}
 </div>
 <h4 class="text-xl  text-white">
   {description}
--- src/pages/ShowCode.svelte
+++ src/pages/ShowCode.svelte
@@ -6,18 +6,18 @@
   import Loading from "../components/Loading.svelte";
   export let id;
   let sdk = {};
-  onMount(() => {
+  let response;
+  let copied = false;
+
+  onMount(async () => {
     sdk = new Appwrite();
     sdk.setEndpoint("http://137.184.150.182:8080/v1").setProject("code-swap");
-  });
-  const fetchUserCode = (async () => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    let promise = sdk.database.getDocument('snippets', id);
-    const response = await promise;
+    response = await sdk.database.getDocument("snippets", id);
     return response;
-  })();
+  });
   const copyUrlPage = () => {
     navigator.clipboard.writeText(window.location.href);
+    copied = true;
   };
 </script>
 
@@ -25,19 +25,19 @@
   class="grid grid-flow-row justify-center gap-5 hover:border-orange-500 w-96 mx-auto"
 >
   <Title />
-  {#await fetchUserCode}
+  {#if response === undefined}
     <Loading text="Loading..." />
-  {:then data}
+  {:else}
     <UserCode
-      code={data.code}
-      title={data.title}
-      description={data.description}
+      code={response.code}
+      title={response.title}
+      description={response.description}
     />
     <button
       class="p-3 border-2 rounded-md hover:border-orange-500 hover:bg-orange-500 hover:text-white transition-all focus:border-orange-500 text-lg text-orange-500"
       on:click={() => {
         copyUrlPage();
-      }}>Swap Code</button
+      }}>{copied ? "Copied" : "Swap Code"}</button
     >
-  {/await}
+  {/if}
 </main>
